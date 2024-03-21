81817fc25af240b0e7b9ad4ecc840f549b66f3c8
Dutra_Nicole <nicoledutra76@gmail.com>
2024-03-21 17:10:39 -0300

    Em Trabalho

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Plataforma_do_Aluno
 {
@@ -10,6 +7,30 @@ namespace Plataforma_do_Aluno
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("Escolha uma operação : \r\n[1] Login \r\n[2] Cadastro");
+                int operação = int.Parse(Console.ReadLine());
+
+                if (operação == 1){
+                    
+                }else if (operação == 2){
+                    Console.WriteLine("Insira o seu e-mail");
+                    string Email = Console.ReadLine();
+                    Console.WriteLine("Insira o nome da sua conta");
+                    string Nome = Console.ReadLine();
+                    Console.WriteLine("Insira a senha da conta");
+                    string Senha = Console.ReadLine();
+
+                    List<Conta> Listaconta = new List<Conta>();
+                    Conta conta = new Conta(Email, Nome, Senha);
+                    Listaconta.Add(conta);
+
+                    
+                }else{
+                    Console.WriteLine("\r\nOperação não existe \r\n");
+                }
+            }
         }
     }
 }
