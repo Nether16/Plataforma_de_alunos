0c2a9a7280f804efc5cfb399cc9e99390118d634
Dutra_Nicole <nicoledutra76@gmail.com>
2024-03-21 15:53:02 -0300

    Adição do progam e da classe da conta do usuario

---
+++ Conta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plataforma_do_Aluno
+{
+    class Conta
+    {
+        private string nome;
+        private string senha;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plataforma_do_Aluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
