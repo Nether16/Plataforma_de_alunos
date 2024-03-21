e432c86d50a83e330111742f22d1a0e28a672b0c
FabianeBortolotti <fabiane.cad@gmail.com>
2024-03-21 17:05:25 -0300

    Adicionando a organização

--- Program.cs
+++ Program.cs
@@ -4,12 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Plataforma_do_Aluno
+namespace plataforma_parte_interna
 {
     class Program
     {
         static void Main(string[] args)
         {
+            List<string> organizacao = new List<string>(); // Cria uma lista vazia
+            int notas;
+
+            while (true)
+            {
+                Console.WriteLine("\nO que você deseja fazer?");
+
+                Console.WriteLine("1 - Organizar tarefas \r\n2 - Ver notas \r\n3 - Acessar Wikipedia");
+                int opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:
+                        Console.WriteLine("\nEssas são suas tarefas:");
+                        foreach (string elemento in organizacao)
+                        {
+                            Console.WriteLine(elemento);
+                        }
+                        Console.Write("Digite a tarefa a ser adicionada: ");
+                        string adicionar = Console.ReadLine();
+                        organizacao.Add(adicionar); // Adiciona o elemento à lista
+                        Console.WriteLine("Tarefa adicionada!");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("\nInforme suas notas para calcularmos quanto você precisa para atingir a média trimestral:");
+                        notas = int.Parse(Console.ReadLine());
+                        break;
+                    default:
+                        Console.WriteLine("Sessão encerrada.");
+                        break;
+                }
+            }
         }
     }
 }
