76d473855be318e5fbe092ed34b3ba482c2673f1
Dutra_Nicole <nicoledutra76@gmail.com>
2024-04-01 16:12:48 -0300

    Update no login

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using static Plataforma_do_Aluno.Conta;
 
 namespace Plataforma_do_Aluno
 {
@@ -7,28 +8,56 @@ namespace Plataforma_do_Aluno
     {
         static void Main(string[] args)
         {
+            List<Conta> Listaconta = new List<Conta>();
+            bool login = true;
+
             while (true)
             {
-                Console.WriteLine("Escolha uma operação : \r\n[1] Login \r\n[2] Cadastro");
-                int operação = int.Parse(Console.ReadLine());
+                if (login)
+                {
+                    Console.WriteLine("Escolha uma operação : \r\n[1] Login \r\n[2] Cadastro");
+                    int operação = int.Parse(Console.ReadLine());
 
-                if (operação == 1){
-                    
-                }else if (operação == 2){
-                    Console.WriteLine("Insira o seu e-mail");
-                    string Email = Console.ReadLine();
-                    Console.WriteLine("Insira o nome da sua conta");
-                    string Nome = Console.ReadLine();
-                    Console.WriteLine("Insira a senha da conta");
-                    string Senha = Console.ReadLine();
+                    if (operação == 1)
+                    {
+                        Console.WriteLine("Insira o seu e-mail");
+                        string Email = Console.ReadLine();
+                        Console.WriteLine("Insira sua senha");
+                        string Senha = Console.ReadLine();
 
-                    List<Conta> Listaconta = new List<Conta>();
-                    Conta conta = new Conta(Email, Nome, Senha);
-                    Listaconta.Add(conta);
+                        foreach (Conta conta in Listaconta)
+                        {
+                            if ((conta.GetEmail() == Email) && (conta.GetSenha() == Senha))
+                            {
+                                login = false;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Email ou a senha ta errado");
+                            }
+                        }
+                    }
+                    else if (operação == 2)
+                    {
+                        Console.WriteLine("Insira o seu e-mail");
+                        string Email = Console.ReadLine();
+                        Console.WriteLine("Insira o nome da sua conta");
+                        string Nome = Console.ReadLine();
+                        Console.WriteLine("Insira a senha da conta");
+                        string Senha = Console.ReadLine();
 
-                    
-                }else{
-                    Console.WriteLine("\r\nOperação não existe \r\n");
+                        Conta conta = new Conta(Email, Nome, Senha);
+                        Listaconta.Add(conta);
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("\r\nOperação não existe \r\n");
+                    }
+                }
+                else
+                {
+                    //Botar o codigo aq
                 }
             }
         }
