30ca14a29b6b01fa5ed58a493f6921436e2fbe07
Dutra_Nicole <134063070+DutraNicole@users.noreply.github.com>
2024-04-04 17:06:41 -0300

    Program.cs
    refazer isso aqui

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using MediawikiNet;
+using Newtonsoft.Json.Linq;
 namespace plataforma_parte_interna
 {
     class Program
@@ -11,7 +12,7 @@ namespace plataforma_parte_interna
         static void Main(string[] args)
         {
             List<string> organizacao = new List<string>(); // Cria uma lista vazia
-            int notas;
+            int nota1, nota2, nota3;
 
             while (true)
             {
@@ -35,9 +36,37 @@ namespace plataforma_parte_interna
                         break;
 
                     case 2:
-                        Console.WriteLine("\nInforme suas notas para calcularmos quanto você precisa para atingir a média trimestral:");
-                        notas = int.Parse(Console.ReadLine());
+                        Console.WriteLine("\nInforme suas notas para calcularmos quanto você precisa para passar de ano:\r\nOBS: Se não tiver a nota informe zero.");
+                        nota1 = int.Parse(Console.ReadLine());
+                        nota2 = int.Parse(Console.ReadLine());
+                        nota3 = int.Parse(Console.ReadLine());
+
+                        int media = 180 - nota1 - nota2 - nota3;
+
+                        if (media >= 0)
+                        {Console.WriteLine("Você precisa de {0} pontos!", media);}
+
+                        else 
+                        {Console.WriteLine("Você já passou!");}
                         break;
+
+                    case 3:
+                            Console.Write("Digite sua pesquisa: ");
+                            string pesquisa = Console.ReadLine();
+
+                            using(WikipediaClient client = new WikipediaClient())
+                        {
+                            WikiSearchRequest req = new WikiSearchRequest(pesquisa);
+                            req.Limit = 5; //We would like 5 results
+                            WikiSearchResponse resp = await client.SearchAsync(req);
+                        }
+
+                            foreach (SearchResult s in resp.QueryResult.SearchResults)
+                            {
+                                Console.WriteLine($" - {s.Title}");
+                            }
+                        break;
+
                     default:
                         Console.WriteLine("Sessão encerrada.");
                         break;
