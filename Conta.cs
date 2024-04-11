c9b2e341a123ccd9634b495c7d2914a7fe1bb5b3
Enzo Nether <133067606+Nether16@users.noreply.github.com>
2024-04-11 16:43:54 -0300

    Termino do sistema de Esqueci a senha

--- Conta.cs
+++ Conta.cs
@@ -8,7 +8,38 @@ namespace Plataforma_do_Aluno
 {
     class Conta
     {
+        private string email;
         private string nome;
         private string senha;
+        public string GetNome()
+        {
+            return email;
+        }
+        public void SetEmail(string email)
+        {
+            this.email = email;
+        }
+        public string GetEmail()
+        {
+            return nome;
+        }
+        public void SetNome(string nome)
+        {
+            this.nome = nome;
+        }
+        public string GetSenha()
+        {
+            return senha;
+        }
+        public void SetSenha(string senha)
+        {
+            this.senha = senha;
+        }
+        public Conta(string Email, string Nome, string Senha)
+        {
+            this.email = Email;
+            this.nome = Nome;
+            this.senha = Senha;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,75 +1,224 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Net.Http;
+using System.IO;
 using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+using System.Net.Mail;
+using System.Net;
 
-namespace plataforma_parte_interna
+namespace Plataforma_do_Aluno
 {
     class Program
     {
         static void Main(string[] args)
         {
-            List<string> organizacao = new List<string>(); // Cria uma lista vazia
-            int nota1, nota2, nota3;
+            List<Conta> Listaconta = new List<Conta>();
+            bool login = true;
 
+            string directoryPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string fileName = "Contas.txt";
+            string filePath = Path.Combine(directoryPath, fileName);
+
+            if (!File.Exists(filePath))
+            {
+                using (FileStream fs = File.Create(filePath)){}
+            }
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                string usuario;
+                while ((usuario = sr.ReadLine()) != null)
+                {
+                    string[] dados = usuario.Split(',');
+                    if (dados[0] == null)
+                    {
+                        continue;
+                    }
+                    Conta conta = new Conta(dados[1], dados[0], dados[2]);
+                    Listaconta.Add(conta);
+                }
+            }
             while (true)
             {
-                Console.WriteLine("\nO que você deseja fazer?");
-
-                Console.WriteLine("1 - Organizar tarefas \r\n2 - Ver notas \r\n3 - Acessar Wikipedia");
-                int opcao = int.Parse(Console.ReadLine());
-
-                switch (opcao)
+                if (login)
                 {
-                    case 1:
-                        Console.WriteLine("\nEssas são suas tarefas:");
-                        foreach (string elemento in organizacao)
+                    Console.WriteLine("Escolha uma operação : \r\n[1] Login \r\n[2] Cadastro");
+                    string operação = Console.ReadLine();
+
+                    if (operação == "1")
+                    {
+                        Console.WriteLine("Insira o seu e-mail");
+                        string Email = Console.ReadLine();
+                        Console.WriteLine("Insira sua senha (Se voce esqueceu a senha digite 'esqueci')");
+                        string Senha = Console.ReadLine();
+
+                        if (Senha != "esqueci")
                         {
-                            Console.WriteLine(elemento);
+                            foreach (Conta conta in Listaconta)
+                            {
+                                if ((conta.GetEmail() == Hash(Email)) && (conta.GetSenha() == Hash(Senha)))
+                                {
+                                    login = false;
+                                }
+                            }
+                            if (login == true) { Console.WriteLine("Email ou Senha Incorretos"); }
                         }
-                        Console.Write("Digite a tarefa a ser adicionada: ");
-                        string adicionar = Console.ReadLine();
-                        organizacao.Add(adicionar); // Adiciona o elemento à lista
-                        Console.WriteLine("Tarefa adicionada!");
-                        break;
+                        else
+                        {
+                            string remetenteEmail = "TrabalhoMosca@outlook.com";
+                            string remetenteSenha = "TrabalhodoMosca";
 
-                    case 2:
-                        Console.WriteLine("\nInforme suas notas para calcularmos quanto você precisa para passar de ano:\r\nOBS: Se não tiver a nota informe zero.");
-                        nota1 = int.Parse(Console.ReadLine());
-                        nota2 = int.Parse(Console.ReadLine());
-                        nota3 = int.Parse(Console.ReadLine());
+                            string destinatarioEmail = Email;
+                            string smtpHost = "smtp.office365.com";
+                            int smtpPort = 587;
 
-                        int media = 180 - nota1 - nota2 - nota3;
+                            try
+                            {
+                                using (SmtpClient smtpClient = new SmtpClient(smtpHost, smtpPort))
+                                {
+                                    smtpClient.Credentials = new NetworkCredential(remetenteEmail, remetenteSenha);
+                                    smtpClient.EnableSsl = true;
 
-                        if (media >= 0)
-                        {Console.WriteLine("Você precisa de {0} pontos!", media);}
+                                    MailMessage message = new MailMessage(remetenteEmail, destinatarioEmail);
+                                    message.Subject = "Esqueci minha senha";
+                                    message.Body = $"A senha é: Chupinga";
+                                    smtpClient.Send(message);
 
-                        else 
-                        {Console.WriteLine("Você já passou!");}
-                        break;
+                                    try
+                                    {
+                                        using (StreamWriter writer = new StreamWriter(filePath, true))
+                                        {
+                                            writer.WriteLine($"{Hash("Nome")} {Hash(Email)} {Hash("Chupinga")}");
+                                        }
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        Console.WriteLine("Um erro ocorreu : " + ex.Message);
+                                    }
+                                    Conta conta = new Conta(Hash("Nome"), Hash(Email), Hash("Chupinga"));
+                                    Listaconta.Add(conta);
 
-                        case 3:
-                        Console.Write("Digite sua pesquisa: ");
-                        string pesquisa = Console.ReadLine();
-                        string url = $"https://pt.wikipedia.org/wiki/{pesquisa}";
+                                    Console.WriteLine($"Email enviado para {Email}");
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine("Erro ao enviar email: " + ex.Message);
+                            }
+                        }
+
+                    }
+                    else if (operação == "2")
+                    {
+                        Console.WriteLine("Insira o seu e-mail");
+                        string Email = Console.ReadLine();
+                        Console.WriteLine("Insira o nome da sua conta");
+                        string Nome = Console.ReadLine();
+                        Console.WriteLine("Insira a senha da conta");
+                        string Senha = Console.ReadLine();
+
+                        string HashEmail = Hash(Email);
+                        string HashNome = Hash(Nome);
+                        string HashSenha = Hash(Senha);
 
                         try
                         {
-                            Process.Start(url);
+                            using (StreamWriter writer = new StreamWriter(filePath, true))
+                            {
+                                writer.WriteLine($"{HashEmail},{HashNome},{HashSenha}");
+                            }
                         }
                         catch (Exception ex)
                         {
-                            Console.WriteLine($"Erro ao abrir o link: {ex.Message}");
+                            Console.WriteLine("Um erro ocorreu : " + ex.Message);
                         }
 
-                        break;
+                        Conta conta = new Conta(HashNome, HashEmail, HashSenha);
+                        Listaconta.Add(conta);
 
-                      default:
-                        Console.WriteLine("Sessão encerrada.");
-                      break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("\r\nOperação não existe \r\n");
+                    }
+
+                    string Hash(string dados)
+                    {
+                        using (SHA256 sha256 = SHA256.Create())
+                        {
+                            byte[] hash = Encoding.UTF8.GetBytes(dados);
+                            StringBuilder sb = new StringBuilder();
+                            foreach (byte b in hash)
+                            {
+                                sb.Append(b.ToString("x2"));
+                            }
+                            string hashString = sb.ToString();
+                            return hashString;
+                        }
+                    }
+                }
+                else
+                {
+                    List<string> organizacao = new List<string>(); // Cria uma lista vazia
+                    int nota1, nota2, nota3;
+
+                    while (true)
+                    {
+                        Console.WriteLine("\nO que você deseja fazer?");
+
+                        Console.WriteLine("1 - Organizar tarefas \r\n2 - Ver notas \r\n3 - Acessar Wikipedia");
+                        int opcao = int.Parse(Console.ReadLine());
+
+                        switch (opcao)
+                        {
+                            case 1:
+                                Console.WriteLine("\nEssas são suas tarefas:");
+                                foreach (string elemento in organizacao)
+                                {
+                                    Console.WriteLine(elemento);
+                                }
+                                Console.Write("Digite a tarefa a ser adicionada: ");
+                                string adicionar = Console.ReadLine();
+                                organizacao.Add(adicionar); // Adiciona o elemento à lista
+                                Console.WriteLine("Tarefa adicionada!");
+                                break;
+
+                            case 2:
+                                Console.WriteLine("\nInforme suas notas para calcularmos quanto você precisa para passar de ano:\r\nOBS: Se não tiver a nota informe zero.");
+                                nota1 = int.Parse(Console.ReadLine());
+                                nota2 = int.Parse(Console.ReadLine());
+                                nota3 = int.Parse(Console.ReadLine());
+
+                                int media = 180 - nota1 - nota2 - nota3;
+
+                                if (media >= 0)
+                                { Console.WriteLine("Você precisa de {0} pontos!", media); }
+
+                                else
+                                { Console.WriteLine("Você já passou!"); }
+                                break;
+
+                            case 3:
+                                Console.Write("Digite sua pesquisa: ");
+                                string pesquisa = Console.ReadLine();
+                                string url = $"https://pt.wikipedia.org/wiki/{pesquisa}";
+
+                                try
+                                {
+                                    Process.Start(url);
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine($"Erro ao abrir o link: {ex.Message}");
+                                }
+
+                                break;
+
+                            default:
+                                Console.WriteLine("Sessão encerrada.");
+                                break;
+                        }
+                    }
                 }
             }
         }
